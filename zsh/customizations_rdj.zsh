c20403b91369137ecf3e44b62ab7875a3860485a
Richard Jordan <richarddjordan@gmail.com>
2015-09-04 11:05:49 -0700

    [RJ][zsh][alias] `pgstart`, `pgstop`

--- zsh/customizations_rdj.zsh
+++ zsh/customizations_rdj.zsh
@@ -1,3 +1,7 @@
 repo() { clear; cd /Repos/$1; }
 _repo() { _files -W /Repos -/; }
 compdef _repo repo
+
+
+alias pgstart="pg_ctl start -D /usr/local/var/postgresql93"
+alias pgstop="pg_ctl stop -D /usr/local/var/postgresql93"

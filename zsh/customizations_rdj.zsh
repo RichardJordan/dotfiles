ec8824c3413d3fc41c9220749022d3bb87d38502
Richard Jordan <richarddjordan@gmail.com>
2016-06-07 13:58:36 -0700

    add gfrom grm grim

--- zsh/customizations_rdj.zsh
+++ zsh/customizations_rdj.zsh
@@ -2,6 +2,9 @@ repo() { clear; cd /Repos/$1; }
 _repo() { _files -W /Repos -/; }
 compdef _repo repo
 
+alias gfrom="git fetch && git reset --hard origin/master"
+alias grm="git rebase master"
+alias grim="git rebase -i master"
 
 alias pgstart="pg_ctl start -D /usr/local/var/postgresql93"
 alias pgstop="pg_ctl stop -D /usr/local/var/postgresql93"
